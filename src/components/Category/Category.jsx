fa656beda4187faf007fdc1ff8e9f924b0063462
Rifazul <rifazul50@gmail.com>
2023-09-25 00:39:03 +0600

    insert the categories data

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,17 +1,21 @@
-
+import PropType from 'prop-types'
 const Category = ({donation}) => {
 const {img,category,text_color,category_bg,card_bg,title} = donation;
  return (
 <div>
-<div className={`card card-compact bg-opacity-{card_bg} border-2`}>
+<div className={`card card-compact   border-2`}>
   <figure><img className="w-full h-[200px] object-cover" src={img} alt="Shoes" /></figure>
   <div className=" pl-2 pb-2 ">
    <p className=" "><button className="py-2 "> {category} </button></p>
-    <h2   className={`card-title text-[${text_color}]`}>  {title} </h2>
+    <h2  className={`card-title bg-[${card_bg}]-500 `}>  {title} </h2>
   </div>
 </div>                                      
 </div>
 );
 };
+
+Category.propType = {
+donation: PropType.object
+}
 
 export default Category;
