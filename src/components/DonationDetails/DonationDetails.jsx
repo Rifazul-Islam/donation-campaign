a28e33dafbcc126ef95bc16508645d9a17d2cfc0
Rifazul <rifazul50@gmail.com>
2023-09-25 09:24:09 +0600

    add the data localStarage save

--- src/components/DonationDetails/DonationDetails.jsx
+++ src/components/DonationDetails/DonationDetails.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { useLoaderData, useParams } from "react-router-dom";
 
 const DonationDetails = () => {
@@ -13,12 +14,44 @@ useEffect(()=>{
   setDonation(findData);
 },[idInt,donations])
 
+const handlerDataSave = ()=>{
+ 
+const donationEmptyArr = [];
+
+const donations = JSON.parse(localStorage.getItem('donations'));
+
+if(!donations){ 
+ donationEmptyArr.push(donation)
+ localStorage.setItem('donations',JSON.stringify(donationEmptyArr))
+ toast.success('your donate Successfully');
+}
+ 
+else{
+
+  const exists = donations.find(item => item.id === idInt)
+  if(!exists){
+  donationEmptyArr.push(...donations,donation)   
+  localStorage.setItem('donations',JSON.stringify(donationEmptyArr))
+  toast.success('your donate Successfully');
+}
+else{
+toast.error('your donate Already added');
+}}};
+
+
+
 return (
 <div className="px-1">
-     <img className="w-full h-[100vh] object-cover" src={donation?.img} alt="" /> 
+    <div className="relative">  <img className="w-full   h-[100vh] object-cover" src={donation?.img} alt="" /> 
+    <div className="w-full p-8 absolute bottom-0   bg-[#0b0b0b80]"> 
+          <button onClick={handlerDataSave} className="bg-orange-500  text-white p-2 rounded"> Donate ${donation?.price} </button>    
+       </div> 
+    
+    </div>
+    
       <h3 className="text-4xl font-bold pt-14 pb-3"> {donation?.title} </h3> 
-
-     <p>{donation?.description}  </p>                          
+      <p>{donation?.description}  </p>  
+      <Toaster />                        
 </div>
 );
 };
