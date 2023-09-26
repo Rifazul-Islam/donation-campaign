8ca38c5d60d2c3c865ea9a12989c7288c3136d39
Rifazul <rifazul50@gmail.com>
2023-09-26 22:11:00 +0600

    add the error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const ErrorPage = () => {
+
               
 return (
-<div>
-            <p> Please Data Add ....</p>                              
+<div className='flex justify-center items-center h-[80vh]'>
+<div className="card w-96 bg-neutral text-neutral-content">
+  <div className="card-body items-center text-center">
+    <h2 className="card-title text-7xl font-bold pb-2"> 404 !!</h2>
+    <p className='text-3xl'> Data is Not Found </p>
+   
+  </div>
+</div>                            
 </div>
 );
 };
